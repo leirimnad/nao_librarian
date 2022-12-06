406e7574ca19c31399433517fc28031f791cf965
Daniil Myronov <myrondan@fit.cvut.cz>
2022-12-06 13:56:31 +0100

    add theoretical scenario

--- client/nao_librarian.py
+++ client/nao_librarian.py
@@ -30,5 +30,31 @@ class NAOLibrarian(object):
                 break
 
     def start_script(self):
-        pass
+        book = self.look_for_book()
+        if book is None:
+            self.on_book_not_found()
+            return
+
+        self.book_found_decorations(book)
+
+        self.run_book_scenario(book)
+
+    def run_book_scenario(self, book):
+        self.go_to_book(book)
+        photo = self.take_book_photo(book)
+
+        # TODO: async call
+        book_info = self.send_photo_to_server(photo)
+
+        if book_info is None:
+            self.on_book_info_not_found()
+            return
+
+        self.say_book_info(book_info)
+
+        self.go_to_box_area()
+
+        self.go_to_box(book_info)
+
+        self.box_found_decorations(book_info)
 
