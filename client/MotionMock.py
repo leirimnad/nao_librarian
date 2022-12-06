0ee07836396cf2e7e3b4f7fd3231287112777fda
Martin Škopek <skopema3@fit.cvut.cz>
2022-12-06 15:55:07 +0100

    keep find logic in loop

--- client/MotionMock.py
+++ client/MotionMock.py
@@ -13,6 +13,12 @@ session = app.session
 server = qi.Session()
 server.connect("tcp://10.10.48.91:9999")
 
+def blink(proxy,flag):
+    if(flag):
+        proxy.fadeRgb(eyes,'#E5FFCC',0.5)
+    else:
+        proxy.fadeRgb(eyes,'#7F00FF',0.5)
+    flag = not flag
 
 def findBooks(img,server,threshold=0.2):
     return False #mock
@@ -45,15 +51,22 @@ motionProxy = ALProxy("ALMotion", robotIP, 9559)
 flag = True
 img = takePicture(session)
 while(findBooks(img,server) == null):
-    for i in range(0,2):
-       blink(proxy)
-        motionProxy.post.moveTo(0, 0, Math.PI/6)
-        
-     motionProxy.post.moveTo(0,0,2/6*Math.PI)
-     
-     for i in range(0,5):
-        bliknk(proxy)
+    for i in range(0,2):#lean 2x30 deg = 60 deg right
+       blink(proxy,flag)
         motionProxy.post.moveTo(0, 0, Math.PI/6)
         img = takePicture(session)
+        if(findBooks(img,server)):
+            break
+     #reset position   
+     motionProxy.post.moveTo(0,0,2/6*Math.PI)
+     
+     for i in range(0,2):
+        blink(proxy,flag)
+        motionProxy.post.moveTo(0, 0, Math.PI/6)
+        img = takePicture(session)
+        if(findBooks(img,server)):
+            break
     motionProxy.post.moveTo(0,0,-2/6*Math.PI)
     motionProxy.post.MoveTo(0,0.5,0)
+
+    
