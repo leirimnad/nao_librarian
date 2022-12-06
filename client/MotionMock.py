b394f9bc1ad9b385b5fba7fcab61acd22a2bed2f
Martin Škopek <skopema3@fit.cvut.cz>
2022-12-06 14:59:57 +0100

    fix typo

--- client/MotionMock.py
+++ client/MotionMock.py
@@ -17,10 +17,10 @@ flag = True
 while(not findBook):
     for i in range(0,2):
         if(flag):
-            proxy.fadeRgb(eyes,'#123456',0.5)
+            proxy.fadeRgb(eyes,'#E5FFCC',0.5)
             flag=not flag
         else:
-            proxy.fadeRgb(eyes,'#145756',0.5)
+            proxy.fadeRgb(eyes,'#7F00FF',0.5)
             flag=not flag
         motionProxy.post.moveTo(0, 0, Math.PI/6)
         
