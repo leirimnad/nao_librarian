d5362621791b37e3a0be9bb9e595d307ec6d3dcb
Daniil Myronov <myrondan@fit.cvut.cz>
2022-12-06 15:36:14 +0100

    add moving based on previous task

--- client/nao_librarian.py
+++ client/nao_librarian.py
@@ -1,12 +1,12 @@
 # coding=utf-8
-import functools
+import copy
 from book import Book, BookInfo
-from tools import get_distance
 
 
 class NAOLibrarian(object):
     def __init__(self, app):
         super(NAOLibrarian, self).__init__()
+        self.foot_len = 3.0 / 100.0
 
         # Get the services ALMemory, ALTextToSpeech.
         self.id = None
@@ -14,6 +14,7 @@ class NAOLibrarian(object):
         session = app.session
         self.memory_service = session.service("ALMemory")
         self.tts = session.service("ALTextToSpeech")
+        self.motion = session.service("ALMotion")
         self.touch = self.memory_service.subscriber("TouchChanged")
 
     def run(self):
@@ -78,7 +79,67 @@ class NAOLibrarian(object):
 
     def go_to_book(self, book):
         # type: (NAOLibrarian, Book) -> None
-        pass
+        self.move_with_stops(book, self.look_for_book)
+
+    def move_to_book(self, book):
+        # type: (NAOLibrarian, Book) -> None
+
+        print("Moving to book: " + str(book))
+
+        self.motion.moveTo(
+            book.image_book.vertical_distance / 100 - self.foot_len,
+            (-1) * book.image_book.horizontal_distance / 100 - self.foot_len,
+            -1 * book.image_book.angle
+        )
+
+    def move_with_stops(self, book, book_func, stops=None, safe_distance=50, min_step_distance=30):
+        # type: (NAOLibrarian, Book, callable, int, int, int) -> None
+        image_book = book.image_book
+        distance_with_stops = max(0, book.distance - safe_distance)
+
+        if stops is None:
+            stops = int(distance_with_stops / min_step_distance)
+
+        if stops != 0 and distance_with_stops / stops < min_step_distance:
+            stops = int(distance_with_stops / min_step_distance)
+            print("Too many stops, reducing to: " + str(stops))
+
+        if stops == 0:
+            self.move_to_book(book)
+            return
+
+        safe_point_mult = distance_with_stops / image_book.distance
+        part_mult = 1.0 / stops
+        stop_mult = safe_point_mult * part_mult
+
+        print("Moving to book: " + str(book) + ", stops left:" + str(stops))
+
+        self.motion.moveTo(
+            (image_book.vertical_distance / 100 - self.foot_len) * stop_mult,
+            ((-1) * image_book.horizontal_distance / 100 - self.foot_len) * stop_mult,
+            (-1 * image_book.angle) * stop_mult
+        )
+
+        if stops <= 0:
+            return
+
+        ideal_image_book = copy.copy(image_book)
+        ideal_image_book.distance = image_book.distance * (1 - stop_mult)
+        ideal_image_book.vertical_distance = image_book.vertical_distance * (1 - stop_mult)
+        ideal_image_book.horizontal_distance = image_book.horizontal_distance * (1 - stop_mult)
+
+        same_book = ideal_image_book.get_same_book(book_func())
+        if same_book is None:
+            print("Did not find the same book, stopping")
+            return
+
+        return self.move_with_stops(
+            book=same_book,
+            book_func=book_func,
+            stops=stops - 1,
+            safe_distance=safe_distance,
+            min_step_distance=min_step_distance
+        )
 
     def take_book_photo(self, book):
         # type: (NAOLibrarian, Book) -> str
