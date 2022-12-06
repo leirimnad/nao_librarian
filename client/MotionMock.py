0319b28c15ea1066492aee8e16cc98e69502bd81
Martin Škopek <skopema3@fit.cvut.cz>
2022-12-06 16:26:46 +0100

    fix syntax errors

--- client/MotionMock.py
+++ client/MotionMock.py
@@ -1,14 +1,14 @@
 # -*- encoding: UTF-8 -*- 
 import sys
-import motion
 import time
+import numpy as np
 from naoqi import ALProxy
 
 def blink(proxy,flag):
     if(flag):
-        proxy.fadeRgb(eyes,'#E5FFCC',0.5)
+        proxy.fadeRgb('FaceLeds','#E5FFCC',0.5)
     else:
-        proxy.fadeRgb(eyes,'#7F00FF',0.5)
+        proxy.fadeRgb('FaceLeds','#7F00FF',0.5)
     flag = not flag
 
 def findBooks(img,server,threshold=0.2):
@@ -23,7 +23,7 @@ def findBooks(img,server,threshold=0.2):
         objects = list(map(lambda o: o[0], res))
         objects = list(set(objects))
         return objects
-    return null
+    return None
 
 def takePicture(session):
     vd = session.service("ALVideoDevice")
@@ -44,30 +44,33 @@ app.start()
 
 session = app.session
 
-
+SERVER_CONNECT_STRING = "tcp://10.10.48.91:9999"
 server = qi.Session()
-server.connect("tcp://10.10.48.91:9999")
+server.connect(SERVER_CONNECT_STRING)
 
 motionProxy = session.service("ALMotion")
+led_proxy = session.service("ALLeds")
 flag = True
 img = takePicture(session)
-while(findBooks(img,server) == null):
+while(findBooks(img,server) == None):
     for i in range(0,2):#lean 2x30 deg = 60 deg right
-       blink(proxy,flag)
-        motionProxy.post.moveTo(0, 0, Math.PI/6)
+        blink(led_proxy,flag)
+        motionProxy.post.moveTo(0, 0, np.pi/6)
         img = takePicture(session)
         if(findBooks(img,server)):
             break
      #reset position   
-     motionProxy.post.moveTo(0,0,2/6*Math.PI)
+    motionProxy.post.moveTo(0,0,2/6*np.pi)
      
-     for i in range(0,2):
-        blink(proxy,flag)
-        motionProxy.post.moveTo(0, 0, Math.PI/6)
+    for i in range(0,2):
+        blink(led_proxy,flag)
+        motionProxy.post.moveTo(0, 0, np.pi/6)
         img = takePicture(session)
         if(findBooks(img,server)):
             break
-    motionProxy.post.moveTo(0,0,-2/6*Math.PI)
+    motionProxy.post.moveTo(0,0,-2/6*np.pi)
     motionProxy.post.MoveTo(0,0.5,0)
+
+
 
     
