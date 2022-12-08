63c66b6fc1075813c555e2d20341a97cbee1f2e9
Martin Škopek <skopema3@fit.cvut.cz>
2022-12-08 13:23:49 +0100

    update server

--- server/server.py
+++ server/server.py
@@ -1,8 +1,61 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import cgi
 import os
+import easyocr
+import datetime
+from pprint import pprint
+import requests
+
 
 class FileUploadHandler(BaseHTTPRequestHandler):
+
+    def get_polygon_area(self,polygon):
+        """Calculate the area of a polygon."""
+        area = 0
+        for i in range(len(polygon)):
+            j = (i + 1) % len(polygon)
+            area += polygon[i][0] * polygon[j][1]
+            area -= polygon[i][1] * polygon[j][0]
+        area = abs(area) / 2.0
+        return area
+        
+    def process_image(self,img_path):
+        reader = easyocr.Reader(['en', 'cs'])
+        print("time")
+        time = datetime.datetime.now()
+        alnum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
+        result = reader.readtext(img_path, allowlist=alnum, batch_size=10, decoder="wordbeamsearch")
+        print(datetime.datetime.now() - time)
+        result = list(map(lambda x: [x[1], self.get_polygon_area([x[0][0], x[0][1], x[0][2], x[0][3]])], result))
+        result.sort(key=lambda x: x[1], reverse=True)
+        pprint(result)
+
+        req = requests.get('https://www.googleapis.com/books/v1/volumes',
+                        params={'q': str(result[0][0]) + " " + str(result[1][0]), "key": "AIzaSyCL1jiXvWMEBBhu1ulEVSgELE_h84IdpqM"})
+        book_id = req.json()['items'][0]['id']
+
+        book = requests.get('https://www.googleapis.com/books/v1/volumes/' + book_id,
+                        params={"key": "AIzaSyCL1jiXvWMEBBhu1ulEVSgELE_h84IdpqM"}).json()['volumeInfo']
+
+        print("Title: " + book['title'])
+
+        if 'authors' in book.keys():
+            print("Authors: " +", ".join(book['authors']))
+        if 'description' in book.keys():
+            print("Description: " + book['description'])
+        if 'categories' in book.keys():
+            print("Categories: " +", ".join(book['categories']))
+        if 'imageLinks' in book.keys() and 'thumbnail' in book['imageLinks'].keys():
+            print("Image: " + book['imageLinks']['thumbnail'])
+        if 'publishedDate' in book.keys():
+            print("Published: " + book['publishedDate'])
+        if 'pageCount' in book.keys():
+            print("Pages: " + str(book['pageCount']))
+        if 'averageRating' in book.keys():
+            print("Rating: " + str(book['averageRating']))
+        if 'language' in book.keys():
+            print("Language: " + book['language'])
+
     def do_POST(self):
         form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type']})
         file_item = form['file']
@@ -10,7 +63,7 @@ class FileUploadHandler(BaseHTTPRequestHandler):
             file_name = os.path.basename(file_item.filename)
             with open('./images/img.png', 'wb') as f:
                 f.write(file_item.file.read())
-
+            self.process_image('./images/img.png')
         # Send a response indicating that the file was uploaded successfully
         self.send_response(200)
         self.end_headers()
